9dbf5d380f5015bdc02c1026c3381106cfae4cfb
leticiafsantos <leticia.fsantos600@gmail.com>
2024-09-05 15:03:11 -0300

    feito2

--- ProjetoAllVoyaje/obj/Debug/net8.0/ProjetoAllVoyaje.AssemblyInfo.cs
+++ ProjetoAllVoyaje/obj/Debug/net8.0/ProjetoAllVoyaje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAllVoyaje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b042b9efca8b2341f98d4a7b9f616d4fbffc84bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAllVoyaje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAllVoyaje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
